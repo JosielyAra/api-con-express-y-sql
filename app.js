95cee301b4579543f2018ba0a5f69e5e1d3cad7c
Josi <jtk.jpaf@gmail.com>
2022-09-16 12:25:22 -0400

    conectado y peticiones a la base de datos

--- app.js
+++ app.js
@@ -7,16 +7,53 @@ const PORT = process.env.PORT || 3050;
 
 const app = express();
 
-app.use(bodyParser().json());
+app.use(bodyParser.json());
 
-//
+//?Create Connect
 const conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
   database : 'my_db'
 });
+//Route
+app.get('/', (req,res) => {
+    res.send('Welcome to my API!');
+});
 
+// all users
+app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM users';
+    conn.query(sql, (error, results) => {
+        if(error) throw error;
+        if(results.length > 0){
+            res.json(results);
+        }else {
+            res.send('Not results')
+        }
+
+    });
+
+});
+
+app.get('/users/:id', (req, res) => {
+    res.send('Get User by Id');
+});
+
+app.post('/add', (req,res) => {
+    res.send('New User');
+});
+
+app.put('/update/:id', (req, res) => {
+    res.send('Update User')
+});
+
+app.delete('/delete/:id', (req, res) => {
+    res.send('Delete User')
+});
+
+
+//Check Connect
 conn.connect(error => {
     if(error) throw error;
     console.log('Database server running!');
